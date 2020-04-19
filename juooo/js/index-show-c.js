9ab0a424d88626b251a3db9b9f67ee538d85e90d
banshi <1303771879@qq.com>
2020-04-19 20:53:55 +0800

    4.19修改数据库提交

--- juooo/js/index-show-c.js
+++ juooo/js/index-show-c.js
@@ -18,3 +18,8 @@ $(".w1204").on("click",".linb", (event)=>{
     $(event.tarage).addClass("active")
     
 })
+$(".pcity-city a").on("click",(event)=>{
+    $(".pcity-city a").removeClass("active-a")
+    console.log(1111,event.tarage)
+    $(event.tarage).addClass("active-a")
+})
